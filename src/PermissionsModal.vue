fdf5ed75ce400f1850cb32e47da7e88777be2df4
Julien Veyssier <eneiluj@posteo.net>
2022-05-19 11:33:59 +0200

    begin to implement permissions modal
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ src/PermissionsModal.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<Modal v-if="open"
+			@close="onClose">
+			Super modal
+		</Modal>
+	</div>
+</template>
+
+<script>
+import Modal from '@nextcloud/vue/dist/Components/Modal'
+
+export default {
+	name: 'PermissionsModal',
+
+	components: {
+		Modal,
+	},
+
+	props: {
+	},
+
+	data() {
+		return {
+			open: false,
+		}
+	},
+
+	watch: {
+	},
+
+	mounted() {
+	},
+
+	methods: {
+		setOpen(val) {
+			this.open = val
+		},
+		onClose() {
+			this.open = false
+			this.$emit('closed')
+		}
+	},
+}
+</script>
