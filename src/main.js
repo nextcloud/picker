98e009b814223e62858653b1ad1a0c922885f578
LepretteN <nathanael.leprette@defis.info>
2024-07-21 18:44:38 +0200

    .setType(5)

--- src/main.js
+++ src/main.js
@@ -107,7 +107,7 @@ function openFilePicker() {
 		// .setMimeTypeFilter(null)
 		.allowDirectories(true)
 		.startAt(lastPath)
-		// .setType(5)
+		.setType(5)
 		.addButton({
 			label: 'Choose this file',
 			callback: (targetPath) => onFileSelected(targetPath),
